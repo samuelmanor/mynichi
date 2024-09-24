26951463856f2304a95c4234c921d2b032a142da
sam <manor@hey.com>
2024-09-24 17:32:22 -0500

    add figma link

--- src/App.js
+++ src/App.js
@@ -14,7 +14,6 @@ stack
   - jest
   - cypress
   - docker
-
 */
 
 function App() {
@@ -25,14 +24,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
