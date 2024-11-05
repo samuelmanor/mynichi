fcf395a7ba0f34ef397b071e4ef735d18e97d367
sam <manor@hey.com>
2024-11-05 14:27:58 -0500

    cleanup

--- README.md
+++
@@ -1,1 +0,0 @@
-[figma file](https://www.figma.com/design/bLfQPWlMqire0Dh4S4uSiE/Untitled?node-id=0-1&node-type=canvas&t=mQbdJu2Yg0h87GIv-0)
--- src/stories/Page.test.jsx
+++
@@ -1,8 +0,0 @@
-import { render, screen } from "@testing-library/react";
-import React from "react";
-
-test("test", () => {
-  render(<div>test</div>);
-  const element = screen.getByText("test");
-  expect(element).toBeDefined();
-});
