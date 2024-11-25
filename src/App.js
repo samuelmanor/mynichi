c1fe5d32ee45647a8bfb18fc59a5e3adcfc9d839
sam <manor@hey.com>
2024-11-25 18:44:04 -0500

    remove unused imports

--- src/App.js
+++ src/App.js
@@ -1,22 +1,17 @@
 import { useEffect } from "react";
 import { Page } from "./components/Page";
 import { NavArrow } from "./components/NavArrow";
-import { useDispatch, useSelector } from "react-redux";
-import { setCurrentPage, setPageCount } from "./reducers/PageReducer";
+import { useDispatch } from "react-redux";
+import { setCurrentPage } from "./reducers/PageReducer";
 import { getFormattedDate } from "./utils/getFormattedDate";
-import { gql, useMutation, useQuery } from "@apollo/client";
-import {
-  FIND_PAGE,
-  ADD_PAGE,
-  GET_PAGE_COUNT,
-  GET_AVAILABLE_PAGES,
-} from "./utils/queries";
+import { useMutation } from "@apollo/client";
+import { ADD_PAGE, GET_PAGE_COUNT, GET_AVAILABLE_PAGES } from "./utils/queries";
 
 function App() {
   const dispatch = useDispatch();
 
   const today = getFormattedDate();
-  const [getTodaysPage, { result, data, loading }] = useMutation(ADD_PAGE, {
+  const [getTodaysPage] = useMutation(ADD_PAGE, {
     refetchQueries: [{ query: GET_PAGE_COUNT, query: GET_AVAILABLE_PAGES }],
     variables: {
       month: today.month,
