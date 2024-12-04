c15c16e8bf39b82554f1aabd90508993e43be26b
sam <manor@hey.com>
2024-12-04 14:37:56 -0500

    remove unnecessary files/folders

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,135 @@
-const app = require("./app"); // the actual Express application
-const config = require("./utils/config");
-const logger = require("./utils/logger");
+// const config = require("./utils/config");
+// const express = require("express");
+// const app = express();
+// const cors = require("cors");
+// const pageRouter = require("./controllers/pages");
+// const middleware = require("./utils/middleware");
+// const logger = require("./utils/logger");
 
-app.listen(config.PORT, () => {
-  logger.info(`Server running on port ${config.PORT}`);
+const { ApolloServer } = require("@apollo/server");
+require("dotenv").config();
+const { startStandaloneServer } = require("@apollo/server/standalone");
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const Page = require("./models/page");
+
+console.log("connecting to", process.env.MONGODB_URI);
+
+mongoose
+  .connect(process.env.MONGODB_URI, { dbName: "notebook" })
+  .then(() => {
+    console.log("connected to MongoDB");
+  })
+  .catch((error) => {
+    console.log("error connecting to MongoDB:", error.message);
+  });
+
+const typeDefs = `
+  type Day {
+    number: Int!
+    name: String!
+  }
+
+  type Date {
+    month: Int!
+    week: Int!
+    day: Day!
+    year: Int!
+  }
+
+  type Page {
+    id: ID!
+    date: Date!
+  }
+
+  type Query {
+    pageCount: Int!
+    findPage(month: Int, dayNum: Int, year: Int, id: String): Page
+    getAvailablePages: [ID]
+  }
+
+  type Mutation {
+    addPage: Page
+  }
+`;
+
+const resolvers = {
+  Query: {
+    pageCount: () => Page.collection.countDocuments(),
+    // pageCount: () => pages.length,
+    // findPage: (root, args) => {
+    //   console.log(args);
+    //   if (args.id) {
+    //     const page = pages.find((page) => page.id === args.id);
+    //     return page;
+    //   } else {
+    //     return pages.find(
+    //       (page) =>
+    //         page.date.month === args.month &&
+    //         page.date.day.number === args.dayNum &&
+    //         page.date.year === args.year
+    //     );
+    //   }
+    // },
+    // getAvailablePages: () => {
+    //   return pages.map((page) => page.id);
+    // },
+  },
+  Mutation: {
+    // addPage: (root, args) => {
+    //   const date = new Date();
+    //   const today = {
+    //     day: {
+    //       number: date.getDate(),
+    //       name: date.toDateString().split(" ")[0].toLowerCase(),
+    //     },
+    //     month: date.getMonth() + 1,
+    //     week: Math.floor((date.getDate() + date.getDay()) / 7) + 1,
+    //     year: date.getFullYear(),
+    //   };
+    //   const pageAlreadyExists = pages.find((p) => {
+    //     return (
+    //       p.date.month === today.month &&
+    //       p.date.day.number === today.day.number &&
+    //       p.date.week === today.week &&
+    //       p.date.year === today.year
+    //     );
+    //   });
+    //   if (pageAlreadyExists) {
+    //     return pageAlreadyExists;
+    //   } else {
+    //     const newPage = {
+    //       id: uuid(),
+    //       date: today,
+    //     };
+    //     pages.push(newPage);
+    //     return newPage;
+    //   }
+    // },
+  },
+};
+
+// app.use(cors());
+// app.use(express.static("dist"));
+// app.use(express.json());
+// app.use(middleware.requestLogger);
+
+// app.use("/api/pages", pageRouter);
+
+// app.use(middleware.unknownEndpoint);
+// app.use(middleware.errorHandler);
+
+// module.exports = app;
+
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+});
+
+startStandaloneServer(server, {
+  listen: { port: process.env.PORT || 4000 },
+}).then(({ url }) => {
+  console.log(`server ready at ${url}`);
 });
